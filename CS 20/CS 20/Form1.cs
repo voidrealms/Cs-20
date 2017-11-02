bab4a26db390f19552571fb1de53eeedbb3395d2
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:23:16 -0400

    First commit

---
+++ CS 20/CS 20/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is called when the button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: C# 20 - First Windows Forms Application
+Published: 9/29/2011 3:38:56 PM
+Link: https://www.youtube.com/watch?v=Oxg6ciIcO3U
+
+Description: Make your first C# Windows forms application in minutes
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
